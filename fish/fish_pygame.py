2d8964bfa9cdbd0021b7631e37a63b092f56403f
Matthew Bennett <matthew.bennett@uclouvain.be>
2021-07-11 13:29:53 +0200

    Use images of fishes

--- fish/fish_pygame.py
+++ fish/fish_pygame.py
@@ -2,6 +2,8 @@ import sys, pygame
 import time
 pygame.init()
 
+path = '/home/mattb/code/python/python_flat/games/fish/'
+
 class Fish():
     def __init__(self, size=20, speed=1, x=0, y=50, color='r'):
         self.size = size
@@ -15,23 +17,22 @@ speed = [2, 0]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
+fish = pygame.image.load(f"{path}fish_green.png")
+fish = pygame.transform.scale(fish, (75, 75))
+fishrect = fish.get_rect()
+fishrect.y = 300
 
-ball = pygame.image.load("fish/block.png")
-ballrect = ball.get_rect()
-ballrect.y = 300
-ballrect.move
-fish = []
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
+    fishrect = fishrect.move(speed)
+    if fishrect.left < 0 or fishrect.right > width:
         speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
+    if fishrect.top < 0 or fishrect.bottom > height:
         speed[1] = -speed[1]
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(fish, fishrect)
     pygame.display.flip()
     time.sleep(0.01)
--- scratch1.py
+++ scratch1.py
@@ -1,10 +1,10 @@
-run('fish_pygame.py')
-run('chimp.py')
-
-pg.examples()
 python3
 def run(file):
     exec(open(file).read())
 def cl():
     import os
     os.system('clear')
+run('fish/fish_pygame.py')
+# run('chimp.py')
+
+exit()
