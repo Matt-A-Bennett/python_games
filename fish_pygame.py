c096210d430d9da0e8e8357689ab21d2f2b38e7b
Matthew Bennett <matthew.bennett@uclouvain.be>
2021-07-11 11:37:10 +0200

    Initial commit

---
+++ fish_pygame.py
@@ -0,0 +1,37 @@
+import sys, pygame
+import time
+pygame.init()
+
+class Fish():
+    def __init__(self, size=20, speed=1, x=0, y=50, color='r'):
+        self.size = size
+        self.speed = speed
+        self.x = x
+        self.y = y
+        self.color = color
+
+size = width, height = 800, 600
+speed = [2, 0]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("/home/mattb/code/python/python_flat/fish_game/block.png")
+ballrect = ball.get_rect()
+ballrect.y = 300
+ballrect.move
+fish = []
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
+    time.sleep(0.01)
---
+++ scratch1.py
@@ -0,0 +1,10 @@
+run('fish_pygame.py')
+run('chimp.py')
+
+pg.examples()
+python3
+def run(file):
+    exec(open(file).read())
+def cl():
+    import os
+    os.system('clear')
