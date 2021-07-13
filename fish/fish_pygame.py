14e89ae5f46b5f0fe695f7edfc06bbc4121cbe84
Matthew Bennett <matthew.bennett@uclouvain.be>
2021-07-13 17:16:30 +0200

    Undo contiuous speed (it was causing a bug)

--- fish/fish_pygame.py
+++ fish/fish_pygame.py
@@ -22,24 +22,24 @@ class Fish_AI():
             self.rect = self.fish.get_rect()
             self.rect.y = np.random.randint(1,height)
             self.rect.x = -150
-            self.speed = [np.random.rand()*5, 0]
+            self.speed = [np.random.randint(1, 5), 0]
         if side == 2:
             self.rect = self.fish.get_rect()
             self.rect.y = np.random.randint(1,height)
             self.rect.x = width
-            self.speed = [-np.random.rand()*5, 0]
+            self.speed = [-np.random.randint(1, 5), 0]
         if side == 3:
             self.fish = pg.transform.rotate(self.image, 270)
             self.rect = self.fish.get_rect()
             self.rect.x = np.random.randint(1,width)
             self.rect.y = height
-            self.speed = [0, -np.random.rand()*5]
+            self.speed = [0, -np.random.randint(1, 5)]
         if side == 4:
             self.fish = pg.transform.rotate(self.image, 90)
             self.rect = self.fish.get_rect()
             self.rect.x = np.random.randint(1,width)
             self.rect.y = -150
-            self.speed = [0, np.random.rand()*5]
+            self.speed = [0, np.random.randint(1, 5)]
 
         self.fish = pg.transform.scale(self.fish, (self.size, self.size))
 
